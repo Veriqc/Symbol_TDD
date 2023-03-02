5de0171dcb2367d12f6be296f28f8df7d9267005
qoohehe <wei.jia.huang.physics@gmail.com>
2023-03-02 10:05:49 +0000

    Added EXP version (under construction)

---
+++ TDD/Exp/EXP.py
@@ -0,0 +1,251 @@
+import numpy as np
+import copy
+import time
+import random
+from graphviz import Digraph
+from IPython.display import Image
+from sympy import *
+from sympy.parsing.sympy_parser import parse_expr
+import sympy as sy
+
+
+"""Define global variables"""
+
+unique_table = dict()
+
+global_index_order = dict()
+
+var_num = 0
+
+epi=0.00000001
+
+
+class BDD:
+    def __init__(self,data=None):
+        """An expression of c exp(\sum{b_i \theta_i})"""
+        if data:
+            self.data=data
+        else:
+            self.data=dict()
+        
+    def __eq__(self,other):
+        if self.data==other.data:
+            return True
+        else:
+            return False
+        
+    def __add__(self, g):        
+        return add(self,g)
+    
+    def __mul__(self, g):
+        return mul(self,g)
+    
+    def __div__(self, g):
+        return normalize_2_fun(g,self)
+    def __sub__(self, g):
+        return self.__add__(g.__mul__(-1))
+        
+def Ini_BDD(index_order=[]):
+    """To initialize the unique_table,computed_table and set up a global index order"""
+    global global_node_idx, var_num
+    
+    set_index_order(index_order)
+    
+    var_num = len(index_order)
+
+def get_one_state():
+    data={tuple([0]*(index_order+1)):1}
+    tdd = BDD(data)
+    return tdd
+
+def get_zero_state():
+    data={tuple([0]*(index_order+1)):0}
+    tdd = BDD(data)
+    return tdd
+
+def get_const_state(w):
+    r=np.abs(w)
+    theta = np.angle(w)
+    key = [0]*(index_order+1)
+    key[-1] = theta
+    data = {tuple(key):r}
+    tdd = BDD(data)
+    return tdd
+
+def set_index_order(var_order):
+    global global_index_order
+    global_index_order=dict()
+    if isinstance(var_order,list):
+        for k in range(len(var_order)):
+            global_index_order[var_order[k]]=k
+    if isinstance(var_order,dict):
+        global_index_order = copy.copy(var_order)
+    global_index_order[-1] = float('inf')
+    
+def get_index_order():
+    global global_index_order
+    return copy.copy(global_index_order)
+    
+def get_int_key(weight):
+    """To transform a complex number to a tuple with int values"""
+    global epi
+    return (int(round(weight.real/epi)) ,int(round(weight.imag/epi)))
+
+
+def get_bdd(f):
+    global global_index_order 
+    if isinstance(f,int) or isinstance(f,float) or isinstance(f,complex):
+        tdd=get_const_state(f)
+        return tdd
+    try:
+        f.args
+    except:
+        tdd=get_const_state(f)
+        return tdd
+    
+    if len(f.args)==0:
+        tdd=get_const_state(complex(f))
+        return tdd
+    
+    
+    #need to be motified
+    
+    if len(f.args)==1:
+        tdd=normalize(str(f),[[1,1,Find_Or_Add_Unique_table(-1)]])
+        return tdd
+    
+    if isinstance(f,sy.add.Add):
+        tdd=get_zero_state()
+        for add_term in f.args:
+            temp_tdd=get_bdd(add_term)
+            tdd=add(tdd,temp_tdd)
+    elif isinstance(f,sy.mul.Mul):
+        tdd=get_one_state()
+        for mul_term in f.args:
+            temp_tdd=get_bdd(mul_term)
+            tdd=mul(tdd,temp_tdd)
+            
+           
+    return tdd
+
+
+def add(tdd1,tdd2):
+    """The apply function of two TDDs. Mostly, it is used to do addition here."""
+    
+    if len(tdd2.data)>len(tdd1.data):
+        return add(tdd2,tdd1)
+    
+    data=copy.copy(tdd1.data)
+    
+    for term in tdd2.data:
+        if term in data:
+            data[term]+=tdd2.data[term]
+        else:
+            data[term]=tdd2.data[term]
+    
+    return BDD(data)
+
+
+
+def mul(tdd1,tdd2):
+    """The contraction of two TDDs, var_cont is in the form [[4,1],[3,2]]"""
+    if not isinstance(tdd1,BDD): 
+        data=tdd2.data.copy()
+        for k in data:
+            data[k]*=tdd1
+        return BDD(data)
+    if not isinstance(tdd2,BDD):
+        data=tdd1.data.copy()
+        for k in data:
+            data[k]*=tdd2
+        return BDD(data)
+    
+    data=dict()
+    
+    for term1 in tdd1.data:
+        for term2 in tdd2.data:
+            t=[k1+k2 for k1,k2 in term1,term2]
+            t=tuple(t)
+            if t in data:
+                data[t]+=tdd1.data[term1]*tdd2.data[term2]
+            else:
+                data[t]=tdd1.data[term1]*tdd2.data[term2]
+
+    return BDD(data)
+        
+
+
+
+
+def normalize_2_fun(tdd1,tdd2):
+
+    return [get_one_state(),tdd1,tdd2]      
+    
+    
+    
+    
+def get_expr(node):
+    if node.key==-1:
+        return 1
+    if node.expr:
+        return node.expr
+    x=node.key
+    
+    expr=parse_expr(x)
+    
+    res=0
+    for succ in node.successor:
+        res+=succ[1]*expr**succ[0]*get_expr(succ[2])
+
+    node.expr=res
+    return res
+
+def get_value_node(node,val,the_key=None):
+    if not the_key:
+        the_key=tuple([tuple([k,val[k]]) for k in val])
+    if the_key in node.value:
+        return node.value[the_key]
+    
+    if node.key==-1:
+        return 1
+    
+    res=0
+    for succ in node.successor:
+        expr=parse_expr(node.key)
+        v=complex(expr.subs(val))
+        res+=succ[1]*v**succ[0]*get_value_node(succ[2],val,the_key)
+
+    node.value[the_key]=res
+    return res
+
+def get_value_node2(node,val,the_key=None):
+    if not the_key:
+        the_key=tuple([tuple([k,val[k]]) for k in val])
+    if the_key in node.value:
+        return node.value[the_key]
+    
+    if node.key==-1:
+        return 1
+    
+    res=0
+
+    v=val[node.key]
+    for succ in node.successor:
+        res+=succ[1]*(v**succ[0])*get_value_node2(succ[2],val,the_key)
+    node.value[the_key]=res
+    return res
+
+def conjugate(tdd):
+    """"change the position of x and y, x<y in this tdd
+    """
+    v=tdd.node.key
+    if v==-1:
+        res=tdd.self_copy()
+        res.weight=tdd.weight.conjugate()      
+        return res
+    the_successor=[]
+    for k in tdd.node.successor:
+        temp_res=conjugate(BDD(k[2]))
+        the_successor.append([k[0],k[1].conjugate()*temp_res.weight,temp_res.node])
+    res=normalize(v,the_successor)
+    return res
--- TDD/TDD_Q.py
+++ TDD/TDD_Q.py
@@ -46,6 +46,230 @@ def get_real_qubit_num(cir):
         q=max(q,max([qbit.index for qbit in gates[k][1]]))
     return q+1
 
+# def cir_2_tn(cir,input_s=[],output_s=[],cong=False):
+#     """return the dict that link every quantum gate to the corresponding index"""
+   
+#     hyper_index=dict()
+#     qubits_index = dict()
+#     start_tensors= dict()
+#     end_tensors = dict()
+    
+#     qubits_num=get_real_qubit_num(cir)
+
+#     for k in range(qubits_num):
+#         qubits_index[k]=0
+        
+#     tn=TensorNetwork([],tn_type='cir',qubits_num=qubits_num)
+
+        
+#     if input_s:
+#         U0=np.array([1,0])
+#         U1=np.array([0,1])
+#         for k in range(qubits_num):
+#             if input_s[k]==0:
+#                 ts=Tensor(U0,[Index('x'+str(k))],'in',[k])
+#             elif input_s[k]==1:
+#                 ts=Tensor(U1,[Index('x'+str(k))],'in',[k])
+#             else:
+#                 print('Only support computational basis input')
+#             tn.tensors.append(ts)
+                
+#     gates=cir.data
+#     for k in range(len(gates)):
+#         from qiskit import version 
+#         if tuple(int(a) for a in version.get_version_info().split('.')) >= (0, 21, 0):
+#             g=(gates[k].operation,gates[k].qubits)
+#         else:
+#             g=gates[k]
+#         nam=g[0].name
+#         # print(g,'\n',nam)
+#         q = [q.index for q in g[1]]
+        
+#         var=[]
+#         # print(g,nam,q)
+
+#         if nam=='cx':
+# #             print(Operator(g[0]).data)
+#             var_con='x'+ str(q[0])+'_'+str(qubits_index[q[0]])
+#             var_tar_in='x'+ str(q[1])+'_'+str(qubits_index[q[1]])
+#             var_tar_out='x'+ str(q[1])+'_'+str(qubits_index[q[1]]+1)
+#             add_hyper_index([var_con,var_tar_in,var_tar_out],hyper_index)
+#             var+=[Index(var_con,hyper_index[var_con]),Index(var_con,hyper_index[var_con]+1),Index(var_tar_in,hyper_index[var_tar_in]),Index(var_tar_out,hyper_index[var_tar_out])]
+#             U=np.array([[1,0,0,0],[0,1,0,0],[0,0,0,1],[0,0,1,0]])
+#             U=reshape(U)
+#             ts=Tensor(U,var,nam,q)
+#             tn.tensors.append(ts)
+#             if qubits_index[q[0]]==0 and hyper_index[var_con]==0:
+#                 start_tensors[q[0]]=ts
+#             if qubits_index[q[1]]==0 and hyper_index[var_tar_in]==0:
+#                 start_tensors[q[1]]=ts
+#             end_tensors[q[0]]=ts
+#             end_tensors[q[1]]=ts
+#             hyper_index[var_con]+=1
+#             qubits_index[q[1]]+=1            
+#             continue
+#         ts=Tensor([],[],nam,q)
+
+
+
+#         '''
+#         TrDD part start
+#         '''
+#         # if len(g[0].params)>0 :
+#         if g[0].is_parameterized():
+#             assert len(g[0].params)==1,'Not Support yet'
+#             param_expr = g[0].params[0] 
+#             # print(param_expr,'\n',type(param_expr))
+            
+#             if isinstance(param_expr,ParameterExpression) and len(param_expr.parameters) != 0:
+#                 '''
+#                 由於旋轉矩陣的三角函數為sin((coeff*theta+const)/2)，分數的角度不好處理，因此實際上是把theta/2當作一個變數。
+#                 下方取出const與coeff，再求出新的表達式new_expr。
+#                 '''
+#                 s=symbols(list(param_expr.parameters)[0].name.replace("[","").replace("]",""))
+#                 sp_expr=sympify(str(param_expr).replace("[","").replace("]",""))
+                
+#                 #取得旋轉角度所加的常數
+#                 const=sp_expr.replace(s,0)
+#                 #取得旋轉角度變數所加的係數
+#                 # coeff=int(format(float(sp_expr.replace(s,1)-const),".0f"))
+#                 coeff=sp_expr.replace(s,1)-const
+#                 coeff_self=int(format(float(sp_expr.replace(s,1)-const),".0f"))
+#                 coeff_plus=int(format(float(sp_expr.replace(s,1)-const),".0f"))+1
+#                 if math.isclose(coeff , coeff_self, rel_tol = 1e-3):
+#                     coeff = coeff_self
+#                 elif math.isclose(coeff , coeff_plus, rel_tol = 1e-3):
+#                     coeff = coeff_plus
+#                 elif math.isclose(coeff+1 , coeff_plus, rel_tol = 1e-3):
+#                     coeff = coeff_plus-1
+#                 else:
+#                     print(sp_expr)
+#                     print(const,coeff)
+#                     print(coeff_self,coeff_plus)
+#                     assert isinstance(coeff,int), 'coeff is %s and the value is %i not int'%(type(coeff),coeff)
+#                 #求出新的變數表達式
+#                 # new_expr=s+const/coeff/2
+#                 new_expr=coeff*s+const/2
+
+#                 # from sympy.simplify.fu import TR9, TR11
+#                 # # sin_expr=nsimplify(TR11(TR9(fourier_series(sin(sp_expr)).truncate())),tolerance=1e-3)
+#                 # # cos_expr=nsimplify(TR11(TR9(fourier_series(cos(sp_expr)).truncate())),tolerance=1e-3)
+
+#                 # sin_expr=nsimplify(TR11(TR9(fourier_series(sin(new_expr)).truncate())),tolerance=1e-3)
+#                 # cos_expr=nsimplify(TR11(TR9(fourier_series(cos(new_expr)).truncate())),tolerance=1e-3)
+
+#                 sin_expr=sin(new_expr).expand(trig=True)
+#                 cos_expr=cos(new_expr).expand(trig=True)
+
+#                 # if coeff > 0:
+#                 #     sins=sin_expr
+#                 #     coss=cos_expr
+#                 # if coeff < 0:
+#                 #     sins=-sin_expr
+#                 #     coss=cos_expr
+
+#                 if nam=='ry':
+#                     U=np.array([[cos_expr,-sin_expr],[sin_expr,cos_expr]])
+#                 if nam=='rz':
+#                     U=np.array([[cos_expr-1j*sin_expr,0],[0,cos_expr+1j*sin_expr]])
+#                     if cong==True:
+#                         U=np.array([[cos_expr+1j*sin_expr,0],[0,cos_expr-1j*sin_expr]])
+#                 if nam=='rx':
+#                     U=np.array([[cos_expr,-1j*sin_expr],[-1j*sin_expr,cos_expr]])
+#                     if cong==True:
+#                         U=np.array([[cos_expr,1j*sin_expr],[1j*sin_expr,cos_expr]])
+
+#                 # if nam=='ry':
+#                 #     U=np.linalg.matrix_power(np.array([[coss,-sins],[sins,coss]]),abs(coeff))
+#                 # if nam=='rz':
+#                 #     U=np.linalg.matrix_power(np.array([[coss-1j*sins,0],[0,coss+1j*sins]]),abs(coeff))
+#                 #     if cong==True:
+#                 #         U=np.linalg.matrix_power(np.array([[coss+1j*sins,0],[0,coss-1j*sins]]),abs(coeff))
+#                 # if nam=='rx':
+#                 #     U=np.linalg.matrix_power(np.array([[coss,-1j*sins],[-1j*sins,coss]]),abs(coeff))
+#                 #     if cong==True:
+#                 #         U=np.linalg.matrix_power(np.array([[coss,1j*sins],[1j*sins,coss]]),abs(coeff))
+            
+#             else:
+#                 # U=Operator(g[0]).data 
+#                 U=g[0].to_matrix()       
+#             '''
+#             TrDD part end
+#             '''
+
+
+#         else:
+#             # U=Operator(g[0]).data 
+#             U=g[0].to_matrix()
+
+#         if is_diagonal(U):
+#             for k in q:
+#                 var_in='x'+ str(k)+'_'+str(qubits_index[k])
+#                 add_hyper_index([var_in],hyper_index)
+#                 var+=[Index(var_in,hyper_index[var_in]),Index(var_in,hyper_index[var_in]+1)]
+#                 if qubits_index[k]==0 and hyper_index[var_in]==0:
+#                     start_tensors[k]=ts
+#                 end_tensors[k]=ts             
+#                 hyper_index[var_in]+=1
+#         else:
+#             for k in q:
+#                 var_in='x'+ str(k)+'_'+str(qubits_index[k])
+#                 var_out='x'+ str(k)+'_'+str(qubits_index[k]+1)
+#                 add_hyper_index([var_in,var_out],hyper_index)
+#                 var+=[Index(var_in,hyper_index[var_in]),Index(var_out,hyper_index[var_out])]
+#                 if qubits_index[k]==0 and hyper_index[var_in]==0:
+#                     start_tensors[k]=ts
+#                 end_tensors[k]=ts                
+#                 qubits_index[k]+=1
+#         if len(q)>1:
+#             U=reshape(U)
+#         if qubits_num>1:
+#             if len(q)==1:
+#                 U=U.T
+#         ts.data=U
+#         ts.index_set=var
+#         tn.tensors.append(ts)
+
+#     for k in range(qubits_num):
+#         if k in start_tensors:
+#             last1=Index('x'+str(k)+'_'+str(0),0)
+#             new1=Index('x'+str(k),0)            
+#             start_tensors[k].index_set[start_tensors[k].index_set.index(last1)]=new1
+#         if k in end_tensors:
+#             last2=Index('x'+str(k)+'_'+str(qubits_index[k]),hyper_index['x'+str(k)+'_'+str(qubits_index[k])])
+#             new2=Index('y'+str(k),0)            
+#             end_tensors[k].index_set[end_tensors[k].index_set.index(last2)]=new2
+               
+#     for k in range(qubits_num):
+#         U=np.eye(2)
+#         if qubits_index[k]==0 and not 'x'+str(k)+'_'+str(0) in hyper_index:
+#             var_in='x'+str(k)
+#             var=[Index('x'+str(k),0),Index('y'+str(k),0)]
+#             ts=Tensor(U,var,'nu_q',[k])
+#             tn.tensors.append(ts)            
+
+#     if output_s:
+#         U0=np.array([1,0])
+#         U1=np.array([0,1])
+#         for k in range(qubits_num):
+#             if input_s[k]==0:
+#                 ts=Tensor(U0,[Index('y'+str(k))],'out',[k])
+#             elif input_s[k]==1:
+#                 ts=Tensor(U1,[Index('y'+str(k))],'out',[k])
+#             else:
+#                 print('Only support computational basis output')
+#             tn.tensors.append(ts)
+    
+#     all_indexs=[]
+#     for k in range(qubits_num):
+#         all_indexs.append('x'+str(k))
+#         for k1 in range(qubits_index[k]+1):
+#             all_indexs.append('x'+str(k)+'_'+str(k1))
+#         all_indexs.append('y'+str(k))
+
+#     return tn,all_indexs
+
+
 def cir_2_tn(cir,input_s=[],output_s=[],cong=False):
     """return the dict that link every quantum gate to the corresponding index"""
    
@@ -54,8 +278,11 @@ def cir_2_tn(cir,input_s=[],output_s=[],cong=False):
     start_tensors= dict()
     end_tensors = dict()
     
+    from qiskit import transpile
+    cir=transpile(cir,basis_gates=['cx','u'],optimization_level=0)
     qubits_num=get_real_qubit_num(cir)
-
+    parameters_dict=dict((v,k) for k, v in dict(enumerate(cir.parameters)).items())
+    parameter_num=len(parameters_dict)
     for k in range(qubits_num):
         qubits_index[k]=0
         
@@ -109,88 +336,58 @@ def cir_2_tn(cir,input_s=[],output_s=[],cong=False):
             qubits_index[q[1]]+=1            
             continue
         ts=Tensor([],[],nam,q)
-        
-        if len(g[0].params)>0 :
-            assert len(g[0].params)==1,'Not Support yet'
-            param_expr = g[0].params[0] 
-            # print(param_expr,'\n',type(param_expr))
-            
-            if isinstance(param_expr,ParameterExpression) and len(param_expr.parameters) != 0:
-                '''
-                由於旋轉矩陣的三角函數為sin((coeff*theta+const)/2)，分數的角度不好處理，因此實際上是把theta/2當作一個變數。
-                下方取出const與coeff，再求出新的表達式new_expr。
-                '''
-                s=symbols(list(param_expr.parameters)[0].name.replace("[","").replace("]",""))
-                sp_expr=sympify(str(param_expr).replace("[","").replace("]",""))
+
+
+        if g[0].is_parameterized():
+            def extract_expr(param):
+                if len(param.parameters)==0:
+                    return None, None, param.sympify().simplify()
+                p=tuple(param.parameters)[0]
+                const=param.bind({p:0})
+                const=const.sympify().simplify()
+                coeff=param.bind({p:1}).sympify()-const
+                coeff=coeff.simplify()
+                return coeff, p, const
+            expr= [extract_expr(item) for item in g[0].params]
+
+            def construct_exp_expr(coeff,p,const):
+                r=np.abs(const)
+                theta = np.angle(const)
+                temp=[0]*parameter_num+[theta]
+                if p:
+                    pos=parameters_dict[p]
+                    temp[pos]=coeff
+                from TDD.Exp.EXP import BDD
+                return BDD({temp:r})
+            # def u3(theta,phi,lam):
+            #     half_theta= theta/2
+            #     cos_half_theta=(exp(I*half_theta)+exp(-I*half_theta))/2
+            #     sin_half_theta=(exp(I*half_theta)-exp(-I*half_theta))/2/I
+            #     return np.array([[cos_half_theta,               -exp(I*lam)*sin_half_theta],
+            #                     [exp(I*phi)*sin_half_theta,      exp(I*(phi+lam))*cos_half_theta]])
+            def angle_mul(theta, scale):
+                return [theta[0]*scale,theta[1],theta[2]*scale]
+
+            def u3_exp(theta,phi,lam):
+                half_theta=angle_mul(theta,I/2)
+                exp_i_half_theta=construct_exp_expr(*half_theta)
+                minus_half_theta= angle_mul(theta,-I/2)
+                exp_minus_i_half_theta=construct_exp_expr(*minus_half_theta)
+
+                from TDD.Exp.EXP import add, mul
+
+                cos_half_theta=mul(0.5,add(exp_i_half_theta,exp_minus_i_half_theta))
+                sin_half_theta=mul(-0.5*I,add(exp_i_half_theta,mul(-1,exp_minus_i_half_theta)))
+
+                exp_i_lam=construct_exp_expr(*lam)
+                exp_i_phi=construct_exp_expr(*phi)
                 
-                #取得旋轉角度所加的常數
-                const=sp_expr.replace(s,0)
-                #取得旋轉角度變數所加的係數
-                # coeff=int(format(float(sp_expr.replace(s,1)-const),".0f"))
-                coeff=sp_expr.replace(s,1)-const
-                coeff_self=int(format(float(sp_expr.replace(s,1)-const),".0f"))
-                coeff_plus=int(format(float(sp_expr.replace(s,1)-const),".0f"))+1
-                if math.isclose(coeff , coeff_self, rel_tol = 1e-3):
-                    coeff = coeff_self
-                elif math.isclose(coeff , coeff_plus, rel_tol = 1e-3):
-                    coeff = coeff_plus
-                elif math.isclose(coeff+1 , coeff_plus, rel_tol = 1e-3):
-                    coeff = coeff_plus-1
-                else:
-                    print(sp_expr)
-                    print(const,coeff)
-                    print(coeff_self,coeff_plus)
-                    assert isinstance(coeff,int), 'coeff is %s and the value is %i not int'%(type(coeff),coeff)
-                #求出新的變數表達式
-                # new_expr=s+const/coeff/2
-                new_expr=coeff*s+const/2
 
-                # from sympy.simplify.fu import TR9, TR11
-                # # sin_expr=nsimplify(TR11(TR9(fourier_series(sin(sp_expr)).truncate())),tolerance=1e-3)
-                # # cos_expr=nsimplify(TR11(TR9(fourier_series(cos(sp_expr)).truncate())),tolerance=1e-3)
-
-                # sin_expr=nsimplify(TR11(TR9(fourier_series(sin(new_expr)).truncate())),tolerance=1e-3)
-                # cos_expr=nsimplify(TR11(TR9(fourier_series(cos(new_expr)).truncate())),tolerance=1e-3)
-
-                sin_expr=sin(new_expr).expand(trig=True)
-                cos_expr=cos(new_expr).expand(trig=True)
-
-                # if coeff > 0:
-                #     sins=sin_expr
-                #     coss=cos_expr
-                # if coeff < 0:
-                #     sins=-sin_expr
-                #     coss=cos_expr
-
-                if nam=='ry':
-                    U=np.array([[cos_expr,-sin_expr],[sin_expr,cos_expr]])
-                if nam=='rz':
-                    U=np.array([[cos_expr-1j*sin_expr,0],[0,cos_expr+1j*sin_expr]])
-                    if cong==True:
-                        U=np.array([[cos_expr+1j*sin_expr,0],[0,cos_expr-1j*sin_expr]])
-                if nam=='rx':
-                    U=np.array([[cos_expr,-1j*sin_expr],[-1j*sin_expr,cos_expr]])
-                    if cong==True:
-                        U=np.array([[cos_expr,1j*sin_expr],[1j*sin_expr,cos_expr]])
-
-                # if nam=='ry':
-                #     U=np.linalg.matrix_power(np.array([[coss,-sins],[sins,coss]]),abs(coeff))
-                # if nam=='rz':
-                #     U=np.linalg.matrix_power(np.array([[coss-1j*sins,0],[0,coss+1j*sins]]),abs(coeff))
-                #     if cong==True:
-                #         U=np.linalg.matrix_power(np.array([[coss+1j*sins,0],[0,coss-1j*sins]]),abs(coeff))
-                # if nam=='rx':
-                #     U=np.linalg.matrix_power(np.array([[coss,-1j*sins],[-1j*sins,coss]]),abs(coeff))
-                #     if cong==True:
-                #         U=np.linalg.matrix_power(np.array([[coss,1j*sins],[1j*sins,coss]]),abs(coeff))
-            
-            else:
-                U=Operator(g[0]).data 
-                # U=g[0].to_matrix()       
-
+                return np.array([[cos_half_theta,              mul(-1.0,exp_i_lam)*sin_half_theta],
+                                [exp_i_phi*sin_half_theta,      exp_i_lam*exp_i_phi*cos_half_theta]])
+            U=u3_exp(*expr)
         else:
-            U=Operator(g[0]).data 
-            # U=g[0].to_matrix()
+            U=g[0].to_matrix()
 
         if is_diagonal(U):
             for k in q:
