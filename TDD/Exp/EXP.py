4014b436eb7ecbcdf4aa5d9a53d094fdfe6f7454
aoowweenn <aoowweenn@gmail.com>
2023-03-09 00:03:04 +0800

    change EXP value format to tuple(r, theta)

--- TDD/Exp/EXP.py
+++ TDD/Exp/EXP.py
@@ -22,78 +22,72 @@ epi=0.00000001
 
 
 class BDD:
-    def __init__(self,data=None):
+    def __init__(self,data=dict()):
         """An expression of c exp(\sum{b_i \theta_i})"""
-        self.data=data if data else dict()
+        self.data = data
+
     @property
     def node(self):
-        # print(self.data,type(self))
-        # from functools import reduce
-        l=[]
-        for k in self.data.keys():
-            l+=list(k)
-            l+=self.data[k]
-        return tuple(l)
-        
-        # return reduce(lambda a,b:a+b , self.data.keys())
+        """Convert to hashable tuple"""
+        print(tuple(self.data.items()))
+        return tuple(self.data.items())
 
     @property
     def weight(self):
+        """Reserved to support current cache table key construction."""
         return 0
+    
     @property
     def is_constant(self):
-        keys = list(self.data.keys())
-        return len(keys)==1 and all(keys[0][i]==0 for i in range (len(keys[0])-1))
+        return all( next(iter(self.data.keys())) ) if len(self.data) == 1 else False
+    
     @property
     def is_zero(self):
-        return self==BDD()
+        return self == BDD()
 
     def __repr__(self) -> str:
         return str(self.data)
 
     def __eq__(self,other):
-        return self.data==other.data
+        return self.data == other.data
         
     def __add__(self, g):        
-        return add(self,g)
+        return add(self, g)
     
     def __mul__(self, g):
-        return mul(self,g)
+        return mul(self, g)
     
     def __div__(self, g):
-        return normalize_2_fun(g,self)
+        return normalize_2_fun(g, self)
+    
     def __sub__(self, g):
         return self.__add__(g.__mul__(-1))
     
     def self_normalize(self, g):
-        return get_one_state(),g , self 
+        return get_one_state(), g, self 
     
         
 def Ini_BDD(index_order=[]):
-    """To initialize the unique_table,computed_table and set up a global index order"""
+    """To initialize the unique_table, computed_table and set up a global index order"""
     global global_index_order, var_num
     
-    # print('EXP 63', index_order)
     set_index_order(index_order)
-    # print('EXP 65', global_index_order)
-    var_num = len(index_order)
 
+    var_num = len(index_order)
 
 def get_one_state():
     #key 是符號項係數，value 是 [r,theta]
-    data={tuple([0]*var_num):[1,0]}
+    data={tuple([0]*var_num):(1,0)}
     tdd = BDD(data)
     return tdd
-
 
 def get_const_state(w):
     #key 是符號項係數，value 是 [r,theta]
     w=complex(w)
-    # print('EXP 79',w)
     r=np.abs(w)
     theta = np.angle(w)
     key = [0]*(var_num)
-    data = {tuple(key):[r,theta]}
+    data = {tuple(key):(r,theta)}
     tdd = BDD(data)
     return tdd
 
@@ -116,12 +110,12 @@ def get_int_key(weight):
     global epi
     return (int(round(weight.real/epi)) ,int(round(weight.imag/epi)))
 
-
 def get_bdd(f):
     global global_index_order 
     if isinstance(f,int) or isinstance(f,float) or isinstance(f,complex):
         tdd=get_const_state(f)
         return tdd
+        
     if isinstance(f,BDD):
         return f
     
@@ -144,7 +138,7 @@ def get_bdd(f):
 
         for key in dict1.keys():
             data[global_index_order[key]]=dict1[key]
-        dict2={tuple(data):[1,c]}
+        dict2={tuple(data):(1,c)}
         tdd = BDD(dict2)
         return tdd
     if isinstance(f,sp.core.add.Add):
@@ -165,12 +159,13 @@ def get_bdd(f):
             tdd=mul(tdd,temp_tdd)
     return tdd
 
-def add_constant(const1:list,const2:list):
+def add_constant(const1:tuple, const2:tuple):
     value=complex(const1[0]*exp(1j*const1[1])+const2[0]*exp(1j*const2[1]))
     r=np.abs(value)
     theta = np.angle(value)
 
-    return [r,theta]
+    return (r, theta)
+
 def add(tdd1,tdd2):
     """The apply function of two TDDs. Mostly, it is used to do addition here."""
     
@@ -213,24 +208,21 @@ def mul(tdd1,tdd2):
     for term1 in tdd1.data:
         not_pop1=True
         if math.isclose(tdd1.data[term1][0],0): #pop the zero terms
-            tdd1.data.copy().pop(term1,None)
             not_pop1=False
         for term2 in tdd2.data:
             not_pop2=True
             if math.isclose(tdd2.data[term2][0],0): #pop the zero terms
-                tdd2.data.copy().pop(term2,None)
                 not_pop2=False
             t=[term1[i]+term2[i] for i in range(len(term1))]
             t=tuple(t)
-            # print('EXP 207', t)
             if not_pop1 and not_pop2:
+                new_r = tdd1.data[term1][0]*tdd2.data[term2][0]
+                new_theta = tdd1.data[term1][1]+tdd2.data[term2][1]
                 if t in data:
-                    data[t][0]*=tdd1.data[term1][0]*tdd2.data[term2][0]
-                    data[t][1]+=tdd1.data[term1][1]+tdd2.data[term2][1]
+                    data[t] = (data[t][0]*new_r, data[t][1]+new_theta)
                 else:
-                    data[t]=[tdd1.data[term1][0]*tdd2.data[term2][0],tdd1.data[term1][1]+tdd2.data[term2][1]]
+                    data[t] = (new_r, new_theta)
 
-    print('EXP 214',data)
     return BDD(data)
         
 
