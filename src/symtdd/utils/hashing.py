5ab151716d60a37863385328e44b2051d7a8a86b
aoowweenn <aoowweenn@gmail.com>
2023-04-19 22:01:43 +0800

    reformat hashing files

--- src/symtdd/utils/hashing.py
+++ src/symtdd/utils/hashing.py
@@ -5,4 +5,8 @@ def hash_double(d: float) -> int:
     """
     https://book.huihoo.com/data-structures-and-algorithms-with-object-oriented-design-patterns-in-c++/html/page217.html
     """
-    return math.floor((2 * math.fabs(math.frexp(d)[0]) - 1) * (2 ** 64 - 1)) if d != 0 else 0
+    return (
+        math.floor((2 * math.fabs(math.frexp(d)[0]) - 1) * (2**64 - 1))
+        if d != 0
+        else 0
+    )
--- tests/test_hashing.py
+++ tests/test_hashing.py
@@ -10,10 +10,14 @@ class TestHashDouble(unittest.TestCase):
         self.assertEqual(hashing.hash_double(0.25), 0)
 
     def test_005(self) -> None:
-        '''
+        """
         (m=0.8, exp=-4) <= frexp(0.05)
         (2|m|-1)*W = (2*0.8-1) * (2**64)
-        '''
-        self.assertNotEqual(hashing.hash_double(0.05), hashing.hash_double(0.05000000000000001))
-        self.assertNotEqual(hashing.hash_double(0.05), hashing.hash_double(0.04999999999999999))
-        self.assertAlmostEqual(hashing.hash_double(0.05), (2 * 0.8 - 1) * (2 ** 64))
+        """
+        self.assertNotEqual(
+            hashing.hash_double(0.05), hashing.hash_double(0.05000000000000001)
+        )
+        self.assertNotEqual(
+            hashing.hash_double(0.05), hashing.hash_double(0.04999999999999999)
+        )
+        self.assertAlmostEqual(hashing.hash_double(0.05), (2 * 0.8 - 1) * (2**64))
