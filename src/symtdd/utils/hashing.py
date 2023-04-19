b9cf9f9d04879d469e10a973d2bcdcf1538f0e3d
aoowweenn <aoowweenn@gmail.com>
2023-04-19 19:50:52 +0800

    Add hashing floating point

---
+++ src/symtdd/utils/hashing.py
@@ -0,0 +1,8 @@
+import math
+
+
+def hash_double(d: float) -> int:
+    """
+    https://book.huihoo.com/data-structures-and-algorithms-with-object-oriented-design-patterns-in-c++/html/page217.html
+    """
+    return math.floor((2 * math.fabs(math.frexp(d)[0]) - 1) * (2 ** 64 - 1)) if d != 0 else 0
---
+++ tests/test_hashing.py
@@ -0,0 +1,19 @@
+import unittest
+
+from symtdd.utils import hashing
+
+
+class TestHashDouble(unittest.TestCase):
+    def test_zero(self) -> None:
+        self.assertEqual(hashing.hash_double(0.0), 0)
+        self.assertEqual(hashing.hash_double(0.5), 0)
+        self.assertEqual(hashing.hash_double(0.25), 0)
+
+    def test_005(self) -> None:
+        '''
+        (m=0.8, exp=-4) <= frexp(0.05)
+        (2|m|-1)*W = (2*0.8-1) * (2**64)
+        '''
+        self.assertNotEqual(hashing.hash_double(0.05), hashing.hash_double(0.05000000000000001))
+        self.assertNotEqual(hashing.hash_double(0.05), hashing.hash_double(0.04999999999999999))
+        self.assertAlmostEqual(hashing.hash_double(0.05), (2 * 0.8 - 1) * (2 ** 64))
